6ddc8d43947a6d9de23fc9a27efe9403b57a4610
Criste Phillips <criste.phillips@yahoo.com>
2022-02-21 16:43:00 -0500

    lesson 3 code review

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations
+    // forward association
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+    // reverse associatin
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
-// use to collect all API routes & package them up
+// this index.js is used to collect all API routes & package them up
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes.js');
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
 
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
