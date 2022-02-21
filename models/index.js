b2e7bb6ff7de06927b2fcf75895675424aa2e766
Criste Phillips <criste.phillips@yahoo.com>
2022-02-21 17:25:25 -0500

    many to many initial functionality

--- models/index.js
+++ models/index.js
@@ -1,14 +1,46 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
-// create associations
-    // forward association
+// create initial associations
+    // initial forward association
 User.hasMany(Post, {
     foreignKey: 'user_id'
 });
-    // reverse associatin
+    // initial reverse association
 Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
-module.exports = { User, Post };
+// create associations that allow User & Post models to query each other in context of a Vote
+// use of both foreign keys prevents 1 user from voting on 1 post many times
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+  });
+  
+ Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+ });
+
+ // associations that enable us to see total number of votes on a post
+ Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+  Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+  });
+  
+  User.hasMany(Vote, {
+    foreignKey: 'user_id'
+  });
+  
+  Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+  });
+
+module.exports = { User, Post, Vote };
