3b91142503a44c5bacf67d236e72d8bf8c6c51e5
Criste Phillips <criste.phillips@yahoo.com>
2022-02-22 13:53:27 -0500

    homepage template updated

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,9 +1,41 @@
-// contains all user-facing routes (homepage & login)
+// home-routes.js contains all user-facing routes (homepage & login)
 
-const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+    Post.findAll({
+        attributes: [
+          'id',
+          'post_url',
+          'title',
+          'created_at',
+          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+              model: User,
+              attributes: ['username']
+            }
+          },
+          {
+            model: User,
+            attributes: ['username']
+          }
+        ]
+      })
+        .then(dbPostData => {
+          // pass a single post object into the homepage template
+          const posts = dbPostData.map(post => post.get({ plain: true }));
+          res.render('homepage', { posts });
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+   });
 });
 
 module.exports = router;
