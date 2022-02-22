5ec846323feaad44215010ecc1e7d1c9577a2e39
Criste Phillips <criste.phillips@yahoo.com>
2022-02-22 12:59:04 -0500

    handlebar view structure initialized

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const path = require('path');
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
-
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({});
+
+// connects to handlebar template; handlebar template lives in view/layouts/main.handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
