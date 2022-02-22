455aecd4085166ff78ef18d81c729af50de47609
Criste Phillips <criste.phillips@yahoo.com>
2022-02-22 17:34:46 -0500

    javascript error fixed

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,7 @@
 // home-routes.js contains all user-facing routes (homepage & login)
-
+const router = require('express').Router();
 const sequelize = require("../config/connection");
-const { Post, User, Comment } = require("../models");
+const { Post, User, Comment, Vote } = require("../models");
 
 router.get("/", (req, res) => {
   Post.findAll({
