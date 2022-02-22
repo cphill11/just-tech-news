0860f0f7cfab17c7a60d380da35ec9f151f22b43
Criste Phillips <criste.phillips@yahoo.com>
2022-02-22 16:51:33 -0500

    added upvote functionality

---
+++ public/javascript/upvote.js
@@ -0,0 +1,31 @@
+// set up button click for upvote button
+
+// define function
+async function upvoteClickHandler(event) {
+  event.preventDefault();
+
+  const id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+
+  const response = await fetch("/api/posts/upvote", {
+    method: "PUT",
+    body: JSON.stringify({
+      post_id: id,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    document.location.reload();
+  } else {
+    alert(response.statusText);
+  }
+}
+
+// query fxn
+document
+  .querySelector(".upvote-btn")
+  .addEventListener("click", upvoteClickHandler);
